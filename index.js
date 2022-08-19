24d5f9967553f258fb0515e07819a0f36ab8383c
hexin17 <hexin17@baidu.com>
2022-08-19 21:55:00 +0800

    hhhh

--- index.js
+++ index.js
@@ -74,26 +74,9 @@ app.post('/message', (req, res) => {
   })
 })
 
-// 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
-
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
-});
-
 // 获取计数
 app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
+  // const result = await Counter.count();
   res.send({
     code: 0,
     data: result,
