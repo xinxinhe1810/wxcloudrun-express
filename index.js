596a71ac6d0cca7913a1b6e8af9a0bc743998805
hexin17 <hexin17@baidu.com>
2022-08-19 21:03:45 +0800

    html

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.get("/api/wx_openid", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 80;
 
 async function bootstrap() {
   // await initDB();
